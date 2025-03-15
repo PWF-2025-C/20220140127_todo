006a61868102d6e8e8dfb398d534a1e5d63a38a9
Alan21103 <syahlansaputra03@gmail.com>
2025-03-15 19:23:50 +0700

    env, mvc, route, relationship

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TodoController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
+Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
+Route::get('/todo/edit', [TodoController::class, 'edit'])->name('todo.edit');
+
+Route::get('/user', [UserController::class, 'index'])->name('user.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -16,5 +24,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
 
 require __DIR__.'/auth.php';
