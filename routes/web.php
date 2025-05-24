a30018d862d75fc1ef56b9ab3688312723a6e126
Alan21103 <syahlansaputra03@gmail.com>
2025-05-24 15:34:42 +0700

    mailtrap

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-Route::middleware('auth')->group(function () {
     Route::get('/todos', [TodoController::class, 'index'])->name('todo.index');
     Route::post('/todo', [TodoController::class, 'store'])->name('todo.store');
     Route::get('/todo/{todo}/edit', [TodoController::class, 'edit'])->name('todo.edit');
---
+++ tugas10.md
@@ -0,0 +1,14 @@
+# Tugas 10
+
+## 1. Browser Register
+![Alt text](screenshot/tugas10/BrowserRegister.png)
+
+## 2. Browser Must Verify Email
+![Alt text](screenshot/tugas10/VerifyEmail.png)
+
+## 3. Browser Mailtrap
+![Alt text](screenshot/tugas10/Mailtrap.png)
+
+## 4. Browser Mailtrap
+![Alt text](screenshot/tugas10/dashboard.png)
+
