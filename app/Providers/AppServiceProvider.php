5892e81b193c2a57cb11abfb40de7e67a6086882
Alan21103 <syahlansaputra03@gmail.com>
2025-05-31 16:27:40 +0700

    login api

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,15 @@
 
 namespace App\Providers;
 
+use Dedoc\Scramble\Scramble;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\Gate; 
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Str;
+use Illuminate\Routing\Route;
+use Laravel\Sanctum\PersonalAccessToken;
+use Laravel\Sanctum\Sanctum;
+
 
 
 
@@ -26,6 +32,9 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useTailwind();
         Gate::define('admin', function ($user){
             return $user->is_admin == true;
+        });
+        Scramble::configure()->routes(function (Route $route){
+            return Str::startsWith($route->uri, 'api/');
         });
     }
 }
---
+++ tugas11.md
@@ -0,0 +1,5 @@
+# Tugas 11
+
+## 1. Login user dengan email dan password
+![Alt text](screenshot/tugas10/BrowserRegister.png)
+
